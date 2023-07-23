49dcb9cf99783cd2e5d62377e898142f07e3dce4
Elias Moura <elias.smourayt@gmail.com>
2023-07-23 14:57:23 -0300

    Utilizando a superclasse título criada.

--- src/br/com/alura/screenmatch/modelos/Filme.java
+++ src/br/com/alura/screenmatch/modelos/Filme.java
@@ -1,40 +1,14 @@
 package br.com.alura.screenmatch.modelos;
 
-public class Filme {
-    private String nome;
-    private int anoDeLancamento;
-    private int duracaoEmMinutos;
-    private boolean incluidoNoPlano;
-    private double somaDasAvaliacoes;
-    private int totalDeAvaliacoes;
+public class Filme(Titulo){
 
-    public void setNome(String nome) {
-        this.nome = nome;
+    private String diretor;
+
+    public String getDiretor() {
+        return diretor;
     }
 
-    public void setAnoDeLancamento(int anoDeLancamento) {
-        this.anoDeLancamento = anoDeLancamento;
-    }
-
-    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
-        this.duracaoEmMinutos = duracaoEmMinutos;
-    }
-
-    public void exibeFichaTecnica() {
-        System.out.printf("br.com.alura.screenmatch.modelos.Filme: %s \n", this.nome);
-        System.out.printf("Ano de lançamento: %d \n", this.anoDeLancamento);
-    }
-
-    public void avalia(double nota) {
-        this.somaDasAvaliacoes += nota;
-        this.totalDeAvaliacoes++;
-    }
-
-    public double pegaMediaAvaliacoes(){
-        return this.somaDasAvaliacoes / this.totalDeAvaliacoes;
-    }
-
-    public int getTotalDeAvaliacoes() {
-        return this.totalDeAvaliacoes;
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
     }
 }
